30484baa27085145c9f4e95717aec08adda8e758
plitche <kysu728@naver.com>
2022-06-15 23:17:13 +0900

    10. JavaScript 문법 - Template Literal

---
+++ nodejs/syntax/Template.js
@@ -0,0 +1,14 @@
+var name = 'egoind';
+var letter = 'Dear '+name+' Lorem \n\nipsum dolor sit amet, consectetur Dear '+name+' adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui Dear '+name+' officia deserunt mollit anim id est laborum.';
+
+var letter = `Dear ${name} Lorem
+
+ipsum dolor sit amet, consectetur Dear ${name} adipisicing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+sunt in culpa qui Dear ${name} officia deserunt mollit anim id est laborum.`;
+
+console.log(letter);
