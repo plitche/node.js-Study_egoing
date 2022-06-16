3090007042a2eee9625700756f97f11143f5d2ee
plitche <kysu728@naver.com>
2022-06-16 22:34:40 +0900

    11. URL의 이해

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,16 +1,23 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url'); // require 요구하다
+// fs, url : 모듈(node.js가 가지고 있는 비슷한 기능들을 모아놓은 것)
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData);
+    console.log(queryData.id);
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+    // console.log(__dirname + url);
+    // response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 
 });
 app.listen(3000);
