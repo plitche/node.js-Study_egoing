17ca7b4dea674e6b25ae69c49f8cc26cf8a61451
plitche <kysu728@naver.com>
2022-06-21 00:01:02 +0900

    29. JavaScript 함수의 출력

--- nodejs/syntax/Function2.js
+++ nodejs/syntax/Function2.js
@@ -2,7 +2,9 @@ console.log(Math.round(1.6));
 console.log(Math.round(1.4));
 
 function sum(first, second) { // parameter: 매게변수
-  console.log(first + second);
+  console.log('a');
+  return first + second;
+  console.log('b');
 }
 
-sum(2,3); // argument: 인자
+console.log(sum(2,3)); // argument: 인자
