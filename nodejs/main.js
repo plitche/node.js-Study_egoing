3986954e0500d8e46763b35605ce62ff378f7a24
plitche <kysu728@naver.com>
2022-06-22 21:43:57 +0900

    30. App 제작 - 함수를 이용하여 정리 정돈하기

--- nodejs/main.js
+++ nodejs/main.js
@@ -3,6 +3,35 @@ var fs = require('fs');
 var url = require('url'); // require 요구하다
 // fs, url : 모듈(node.js가 가지고 있는 비슷한 기능들을 모아놓은 것)
 
+function templateHTML(title, list, body) {
+  return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+    </body>
+    </html>
+  `;
+}
+
+function templateList(filelist) {
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i++;
+  }
+  list = list + '</ul>';
+
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -22,63 +51,17 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist) {
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i++;
-          }
-          list = list + '</ul>';
-
-          var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-          `;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
           response.writeHead(200);
           response.end(template);
         })
       } else {
         fs.readdir('./data', function(error, filelist) {
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i++;
-          }
-          list = list + '</ul>';
-
           fs.readFile('data/${queryData.id}', 'utf-8', function(err, description) {
             var title = queryData.id;
-            var template = `
-              <!doctype html>
-              <html>
-              <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-              </head>
-              <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-              </body>
-              </html>
-            `;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
             response.writeHead(200);
             response.end(template);
           });
