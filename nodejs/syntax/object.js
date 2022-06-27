1033eda1e12ef6fd32f908b929a71525859b763b
plitche <kysu728@naver.com>
2022-06-28 00:03:46 +0900

    46. JavaScript 객체 반복

--- nodejs/syntax/object.js
+++ nodejs/syntax/object.js
@@ -1,9 +1,22 @@
 var members = ['egoing', 'k8805', 'hoya'];
 console.log(members[1]); // k8805
 
+var i = 0;
+while(i < members.length) {
+  console.log('array loop', members[i]);
+  i = i +1;
+}
+
 var roles = {
   'programmer':'egoing',
   'desinger':'k8805',
   'manager':'hoya'
 };
+
+for (var name in roles) {
+  console.log('object => ', name); //key
+  console.log('value => ', roles[name]); //value
+}
+
 console.log(roles.desinger); // k8805
+console.log(roles.['desinger']); // k8805
