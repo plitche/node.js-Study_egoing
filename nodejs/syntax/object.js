bdaee28be37f96efede3cc454702abf1db30fb35
plitche <kysu728@naver.com>
2022-06-27 23:57:58 +0900

    45. JavaScript 객체의 형식

---
+++ nodejs/syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]); // k8805
+
+var roles = {
+  'programmer':'egoing',
+  'desinger':'k8805',
+  'manager':'hoya'
+};
+console.log(roles.desinger); // k8805
