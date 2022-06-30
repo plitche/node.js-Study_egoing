5beebbe6354cac8ff04fedef3d8f7901fc365690
plitche <kysu728@naver.com>
2022-06-30 22:17:45 +0900

    55. App 제작 - 출력정보에 대한 보안

--- nodejs/main.js
+++ nodejs/main.js
@@ -4,6 +4,7 @@ var url = require('url'); // require 요구하다
 var qs = require('querystring');
 var template = require('./lib/template/js');
 var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 // fs, url : 모듈(node.js가 가지고 있는 비슷한 기능들을 모아놓은 것)
 
 var app = http.createServer(function(request,response){
@@ -36,13 +37,17 @@ var app = http.createServer(function(request,response){
           var filteredId = path.parse(queryData.id).base;
           fs.readFile('data/${filteredId}', 'utf-8', function(err, description) {
             var title = queryData.id;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+              allowedTags: ['h1']
+            });
             var list = template.list(filelist);
-            var html = template.html(title, list,
-              `<h2>${title}</h2><p>${description}</p>`
+            var html = template.html(sanitizedTitle, list,
+              `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`
               , `<a href="/create">create</a>
-              <a href="/update?id=${title}">update</a>
+              <a href="/update?id=${sanitizedTitle}">update</a>
               <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${title}">
+                <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
               </form>`
             );
